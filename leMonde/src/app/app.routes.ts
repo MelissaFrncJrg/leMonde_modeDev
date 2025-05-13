edd6bc87b0ca69ef464d3675a27d0fb40136f59f
Alexis Chaignon <alexis.chaignon@gmail.com>
2025-05-13 13:45:29 +0200

    feat: add article route

--- leMonde/src/app/app.routes.ts
+++ leMonde/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component:HomePageComponent},
+  { path: 'article', component: ArticleDetailComponent },
 ];
