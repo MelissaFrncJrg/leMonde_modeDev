9f9c00981a83fa5ce8868562e32640ead74da912
coralie <russicoralie812@gmail.com>
2025-05-13 12:18:39 +0200

    create-navbar

--- leMonde/src/app/app.component.ts
+++ leMonde/src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   template: '<router-outlet></router-outlet>',
 })
 
 export class AppComponent {
-  title = 'leMonde';
+  title = 'angular';
+  compteurParent = 1;
+  isVisible = false;
+  majCompteur(event: number) {
+    this.compteurParent += event;
+    console.log(this.compteurParent);
+  }
+  submit() {
+    this.isVisible = true;
+    console.log("submit");
+  }
 }
