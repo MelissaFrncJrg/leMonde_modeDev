a751161fbf63bd56beb3cb7ac37881bdb9a977b5
vlerOux <vleroux9738@gmail.com>
2025-05-13 11:51:12 +0200

    display component.html

--- leMonde/src/app/home-page/home-page.component.ts
+++ leMonde/src/app/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  standalone :true,
+  imports: [CommonModule],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
 
